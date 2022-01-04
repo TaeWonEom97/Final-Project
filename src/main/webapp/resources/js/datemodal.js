cd8a7605d1fda094c54194435d7008c79f97a5e5
Isenring <pepelong97@naver.com>
2022-01-04 16:59:33 +0900

    datemodal 날짜 alert창 주석처리

--- src/main/webapp/resources/js/datemodal.js
+++ src/main/webapp/resources/js/datemodal.js
@@ -29,7 +29,7 @@ $( function() {
 
 //날짜 조회
 
-$(function(){
+/*$(function(){
    // 조회를 클릭하면 조회내용 보여주기
    $("#select").click(function(e){
       e.preventDefault();
@@ -43,22 +43,7 @@ $(function(){
       if(date1 === ''){
          alert('날짜를 입력해 주세요');
          return;         
-      }
+      }*/
 
 
 	
-/*날짜를 입력하지 않으면 "날짜를 입력 해 주세요 "모달창 띄우기
- <!--Modal--> 
-<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h3 id="myModalLabel">Modal header</h3>
-    </div>
-    <div class="modal-body">
-        <p>One fine body…</p>
-    </div>
-    <div class="modal-footer">
-        <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-        <button class="btn btn-primary">Save changes</button>
-    </div>
-</div> 
